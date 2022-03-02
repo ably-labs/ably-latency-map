2a374d421526b95b917badbf43b7c06cdb6bc947
Lewis Marshall <lewis.marshall@ably.com>
2022-03-02 19:11:23 +0000

    Update bot to calculate latency
    Signed-off-by: Lewis Marshall <lewis.marshall@ably.com>

--- bot/index.js
+++ bot/index.js
@@ -1,28 +1,68 @@
 const Ably = require('ably');
 
-const info = {
+const me = {
   id: 'eu-west-1',
   city: 'Ireland',
   coordinates: [-8, 53 ],
 };
 
+const client = new Ably.Realtime({
+  authUrl: 'https://ably.com/ably-auth/token/demos',
+  clientId: me.id,
+});
+
+const channel = client.channels.get('ably-latency-map');
+
+const cleanup = () => {
+  channel.presence.leave();
+  client.close();
+};
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
+
 const main = async () => {
-  const client = new Ably.Realtime({
-    authUrl: 'https://ably.com/ably-auth/token/demos',
-    clientId: info.id,
-  });
-
-  const channel = client.channels.get('ably-latency-map');
-
-  console.log(`Entering channel: ${JSON.stringify(info)}`);
+  console.log(`Entering channel: ${JSON.stringify(me)}`);
   try {
-    await channel.presence.enter(info);
+    await channel.presence.enter(me);
   } catch (err) {
     console.error(`Error entering channel: ${err}`);
     return;
   }
 
   console.log('Waiting for instructions...');
+  channel.subscribe(({ name, data }) => {
+    console.log(`Received message: name="${name}" data="${JSON.stringify(data)}"`);
+
+    switch (name) {
+      case 'request':
+        if(data.botId === me.id) {
+          const ping = {
+            seq: data.seq,
+            src: me.id,
+            time: Date.now(),
+          };
+          console.log(`Sending ping: ${JSON.stringify(ping)}`);
+          channel.publish('ping', ping);
+        }
+        break;
+      case 'ping':
+        const pong = {
+          seq: data.seq,
+          src: data.src,
+          dst: me.id,
+          diff: Date.now() - data.time,
+        };
+        console.log(`Sending pong: ${JSON.stringify(pong)}`);
+        channel.publish('pong', pong);
+        break;
+      case 'pong':
+        // just ignore pongs
+        break;
+      default:
+        console.error(`Unhandled message: ${name}`);
+    };
+  });
 };
 
 main();
